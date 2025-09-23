4ff8afd04bdfcefd59c29685c7113a2e138336b3
mnasonovy <mnasonovy@gmail.com>
2025-09-23 04:24:34 +0400

    Add Publisher entity

---
+++ Library.Domain/Models/Publisher.cs
@@ -0,0 +1,8 @@
+namespace Library.Domain.Models;
+
+// Publisher reference
+public class Publisher
+{
+    public int Id { get; set; }                  // Primary key
+    public string Name { get; set; } = "";       // Publisher name (e.g., "Eksmo")
+}
