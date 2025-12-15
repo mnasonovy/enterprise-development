4eca2ea4554fe923a9bcf58ba542e8896d4c3150
mnasonovy <mnasonovy@gmail.com>
2025-12-15 14:40:12 +0400

    feat(infrastructure): add MongoDbContext and book repository

---
+++ Library.Infrastructure.MongoDb/Database/MongoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Library.Domain.Models;
+using MongoDB.Driver;
+
+namespace Library.Infrastructure.MongoDb.Database;
+
+public class MongoDbContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDbContext(string connectionString, string databaseName)
+    {
+        var client = new MongoClient(connectionString);
+        _database = client.GetDatabase(databaseName);
+    }
+
+    public IMongoCollection<Book> Books => _database.GetCollection<Book>("books");
+    public IMongoCollection<Author> Authors => _database.GetCollection<Author>("authors");
+    public IMongoCollection<Reader> Readers => _database.GetCollection<Reader>("readers");
+    public IMongoCollection<Issue> Issues => _database.GetCollection<Issue>("issues");
+}
