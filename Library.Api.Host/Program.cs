16590dcb7d93ceb389f7ae1e95a13bb6f3e65444
mnasonovy <mnasonovy@gmail.com>
2025-12-15 15:35:38 +0400

    feat(api): add Library.Api.Host with MongoDB DI and Swagger

---
+++ Library.Api.Host/Program.cs
@@ -0,0 +1,49 @@
+﻿using Library.Application.Services;
+using Library.Infrastructure.MongoDb.Database;
+using Library.Infrastructure.MongoDb.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// MongoDB Configuration
+var mongoSettings = builder.Configuration.GetSection("MongoDbSettings");
+var connectionString = mongoSettings.GetValue<string>("ConnectionString")
+    ?? throw new InvalidOperationException("MongoDB ConnectionString is not configured");
+var databaseName = mongoSettings.GetValue<string>("DatabaseName")
+    ?? throw new InvalidOperationException("MongoDB DatabaseName is not configured");
+
+// Register MongoDB context as singleton
+builder.Services.AddSingleton(new MongoDbContext(connectionString, databaseName));
+
+// Register repositories as scoped
+builder.Services.AddScoped<BookMongoRepository>();
+builder.Services.AddScoped<AuthorMongoRepository>();
+builder.Services.AddScoped<ReaderMongoRepository>();
+builder.Services.AddScoped<IssueMongoRepository>();
+
+// Register application services as scoped
+builder.Services.AddScoped<BookService>();
+builder.Services.AddScoped<AuthorService>();
+builder.Services.AddScoped<ReaderService>();
+builder.Services.AddScoped<IssueService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
